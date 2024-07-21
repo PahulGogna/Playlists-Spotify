2944e178291c6368ab0b236a2f61f6e20298dcf5
Pahul Gogna <135852041+PahulGogna@users.noreply.github.com>
2024-07-21 14:24:58 +0530

    g

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,93 @@
-import { useState, useEffect } from 'react'
-import TotalPlaylists from './TotalPlaylists'
-import Filter from './Filter'
-import Playlist from './Playlist'
-import TotalHours from './TotalHours'
+import { useState, useEffect } from 'react';
+import TotalPlaylists from './TotalPlaylists';
+import Filter from './Filter';
+import Playlist from './Playlist';
+import TotalHours from './TotalHours';
+import queryString from 'query-string';
 
 function App() {
-    const fakeSeverData = {
-        user:{
-            name:'D',
-            playlists:[
-                {name:'Liked', 
-                    songs: [{name:"song1", duration:1453},
-                            {name: "song2", duration:324},
-                            {name:'song3', duration:2341}]
-                },
-                {name: 'New Releases',
-                    songs: [{name:"song4", duration:1453},
-                            {name: "song5", duration:324},
-                            {name:'song6', duration:2341}]
-                },
-                {name: 'Saved',
-                    songs: [{name:"song7", duration:1453},
-                            {name: "song8", duration:324},
-                            {name:'song9', duration:2341}]
-                },
-                {name: 'Downloads',
-                    songs: [{name:"song10", duration:1453},
-                            {name: "song11", duration:324},
-                            {name:'song12', duration:2341}]
-                },
-            ]
-        }
-    }
-
-    const [serverData, setServerData] = useState({});
+    let searchParams = {};
+    const [serverData, setServerData] = useState({ user: {} });
     const [filterString, setFilterString] = useState('');
 
+    function requestData() {
+        // setServerData()
+    }
 
-    useEffect( () => {
-        setTimeout(
-            () => {setServerData(fakeSeverData)}, 500
-        )
-    },[]);
+    let filteredPlaylists = serverData.user && serverData.user.playlists ? serverData.user.playlists.filter(playlist => {
+        let names = playlist.name.toLowerCase().includes(filterString);
+        let songs = false;
+        for (let i = 0; i < playlist.songs.length; i++) {
+            if (playlist.songs[i].name.toLowerCase().includes(filterString)) {
+                songs = true;
+                break;
+            }
+        }
+        return names || songs;
+    }
+    ) : [];
 
-    const hours = serverData.user ? serverData.user.playlists.reduce(function(sum, currentPlaylist){
-                    for(let i = 0; i < currentPlaylist.songs.length; i++){
-                        sum += (currentPlaylist.songs[i].duration)
-                    }
-                    return sum
-                },0):0;
-    
-    
-    return ( serverData.user ? <div className='App'>
-                <h1>{(serverData.user.name + "'s Playlists" || "Loading")}</h1>
-                <TotalPlaylists total={serverData.user.playlists.length}/>
-                <TotalHours hours = {hours}/>
-                {<Filter onTextChange={text => setFilterString(text.toLowerCase())}/>}
-                {serverData.user.playlists.filter(playlist => {
-                    let names = playlist.name.toLowerCase().includes(filterString)
-                    let songs = false
-                    for (let i = 0; i < playlist.songs.length; i++){
-                        if (playlist.songs[i].name.toLowerCase().includes(filterString)){
-                            songs = true
-                            break
-                        }
-                    }
-                    return names || songs
+    const hours = filteredPlaylists ? filteredPlaylists.reduce((sum, currentPlaylist) => {
+        for (let i = 0; i < currentPlaylist.songs.length; i++) {
+            sum += (currentPlaylist.songs[i].duration);
+        }
+        return sum;
+    }, 0) : 0;
+
+    useEffect(() => {
+        const searchParams = queryString.parse(window.location.search);
+        let accessToken = searchParams.access_token;
+
+        const fetchUserData = fetch('https://api.spotify.com/v1/me', {
+            headers: {
+                Authorization: 'Bearer ' + accessToken
+            }
+        }).then(response => response.json());
+
+        const fetchUserPlaylists = fetch('https://api.spotify.com/v1/me/playlists', {
+            headers: {
+                Authorization: 'Bearer ' + accessToken
+            }
+        }).then(response => response.json());
+
+        Promise.all([fetchUserData, fetchUserPlaylists]).then(([userData, playlistsData]) => {
+            let responsePlaylists = [];
+            for (let i = 0; i < playlistsData.total; i++) {
+                responsePlaylists.push({ name: playlistsData.items[i].name, songs: [] , image: playlistsData.items[i].images ? playlistsData.items[i].images[0].url:''}); 
+                console.log(playlistsData.items[i].images)
+            }
+
+            setServerData({
+                user: {
+                    name: userData.display_name,
+                    playlists: responsePlaylists
                 }
-                ).map((playlist, index)=>
-                    <Playlist name={playlist.name} songs={playlist.songs.map((value)=> {
-                        return value.name
-                    })} key={index}/>
+            });
+        }).catch(error => {
+            console.error('Error fetching data:', error);
+        });
+
+    }, []);
+
+    return (
+        serverData && serverData.user && serverData.user.name ?
+            <div className='App'>
+                <h1>{serverData.user.name + "'s Playlists" || "Loading"}</h1>
+                <TotalPlaylists total={filteredPlaylists.length} />
+                <TotalHours hours={hours} />
+                <Filter onTextChange={text => setFilterString(text.toLowerCase())} />
+                {filteredPlaylists.map((playlist, index) =>
+                    <Playlist name={playlist.name} songs={playlist.songs.map((value) => value.name)} image={playlist.image} key={index} />
                 )}
-            </div> : <h1 style={{textAlign:'center'}}> Loading... </h1>
-        )
+            </div>
+            : searchParams ?
+                <h1 style={{ textAlign: 'center' }}>Loading... {requestData()}</h1>
+                : <h1 style={{ textAlign: 'center' }}>
+                    <button className='Sign-in-button' onClick={() => window.location = 'http://localhost:8888/login/'}>
+                        <h3>Sign in to Spotify</h3>
+                    </button>
+                </h1>
+    );
 }
 
-export default App
+export default App;
